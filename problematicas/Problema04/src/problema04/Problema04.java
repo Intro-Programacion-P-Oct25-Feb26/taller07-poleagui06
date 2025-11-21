8523ece838e99cb245cc73cd11f2b19409f02226
poleagui06 <jeanpoleagui06@gmail.com>
2025-11-21 00:16:07 -0500

    Problema_04

--- problematicas/Problema04/src/problema04/Problema04.java
+++ problematicas/Problema04/src/problema04/Problema04.java
@@ -14,28 +14,35 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double contador = 0;
+        int contador = 0;
+        int denominador = 1;
+        
         double resultado = 0;
-        int numerador = 1;
-        int contadorSigno = 1;
-        String signo = "";
+        double valor;
+        
         String mensaje = "";
-
-        while (contador <= 15) {
-            if (contadorSigno % 2 == 0) {
-                signo = "-";
-            } else {
-                signo = "+";
-            }
-            resultado = (contador + 1);
-            mensaje = String.format("%s%s%d/%.0f ", mensaje,signo,
-                    numerador, resultado);            
-            contador = contador + 2;
-            contadorSigno = contadorSigno + 1;
+        boolean positivo = true; // al ser true se suma
 
 
+        while (contador < 8) {
+            valor = 1.0 / denominador;
+            if (positivo) { // aqui se sumaria el valor
+                resultado = resultado + valor;
+                if (contador == 0){
+                mensaje = "1";
+                } else{
+                mensaje = String.format("%s + 1/%d", mensaje, denominador);            
+                  }
+                
+            } else { // aqui se resta el valor
+                resultado = resultado - valor;
+                mensaje = String.format("%s - 1/%d", mensaje, denominador);
+              }
+            denominador = denominador + 2;
+            positivo = positivo != true; // esto altera los signos en cada cada cadena
+            contador = contador + 1;
         }
-        System.out.printf("%s", mensaje); // TODO code application logic here
+        System.out.printf("%s = %.5f%n", mensaje, resultado); // TODO code application logic here
     }
 
 }
